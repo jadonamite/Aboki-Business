a74888ecf5e2cf4664a6c2c46e4f15a21b6b50af
Jadonamite <jadonamite@gmail.com>
2025-06-22 19:31:33 +0100

    server side rendering common

---
+++ src/components/common/NoSSR.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const NoSSR = ({ children, fallback = null }) => {
+   const [hasMounted, setHasMounted] = useState(false);
+
+   useEffect(() => {
+      setHasMounted(true);
+   }, []);
+
+   if (!hasMounted) {
+      return fallback;
+   }
+
+   return children;
+};
+
+export default NoSSR;
